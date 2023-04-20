c337c69404c61903f30b9b24e8e64c2c7ce82f7d
JuanRivera-7 <62785733+JuanRivera-7@users.noreply.github.com>
2023-04-20 14:29:40 -0700

    Update menuNavigator.js
    Updated menu navigation => updated colors

--- src/navigators/menuNavigator.js
+++ src/navigators/menuNavigator.js
@@ -1,14 +1,27 @@
-import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import Home from '../screens/Home/Home.js';
+import AccountSetting from '../screens/Home/AccountSetting.js';
+import Events from '../screens/Home/Events.js';
+import Organization from '../screens/Home/Organization.js';
+import colors from '../config/colors.js';
+import NewPass from '../screens/Home/NewPass.js';
 
-const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
 
-function menuNavigator() {
+export default function MenuNav() {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="AccountSetting" component={AccountSetting} />
-      <Stack.Screen name="Organization" component={Organization} />
-      <Stack.Screen name="Events" component={Events} />
-    </Stack.Navigator>
+      
+      <Drawer.Navigator initialRouteName="Home" useLegacyImplementation screenOptions={{
+        headerTintColor:colors.third,
+        drawerActiveTintColor: colors.third,
+        drawerInactiveTintColor: "#fff",
+        headerStyle:{backgroundColor: colors.secondary},
+         drawerStyle: { backgroundColor: colors.secondary} }}>
+        <Drawer.Screen name="Home" component={Home} />
+        <Drawer.Screen name="Account Settings" component={AccountSetting}/>
+        <Drawer.Screen name="Events" component={Events} />
+        <Drawer.Screen name="Organization" component={Organization} />
+        <Drawer.Screen name="Change Password" component={NewPass} />
+      </Drawer.Navigator>
   );
 }
