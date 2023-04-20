8588a981d490a4d8ccbcf062390c66cd6e186538
JuanRivera-7 <62785733+JuanRivera-7@users.noreply.github.com>
2023-04-20 14:34:02 -0700

    Update Organization.js
    Made it scroll view

--- src/screens/Home/Organization.js
+++ src/screens/Home/Organization.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text,ScrollView } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import colors from '../../config/colors';
 
-function Home(props) {
+function Organization(props) {
     return (
-        <Text> Organization </Text>
+        <SafeAreaView style ={{backgroundColor: colors.primary, flex:1}}>
+            <ScrollView>
+                <Text style={{color: colors.third}}>Organized</Text>
+            </ScrollView>
+        </SafeAreaView>
     );
 }
 
-export default Home;
+export default Organization;
