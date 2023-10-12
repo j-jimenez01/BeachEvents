e23de4f663531a9fd59c2825f07213f1b02dfa75
NoMamesWey1 <josejimenez530@yahoo.com>
2023-10-12 12:19:52 -0700

    juan to main

--- src/navigators/LogNav.js
+++ src/navigators/LogNav.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import routes from '../config/routes';
 import { Forgot, Login, MenuNav, Register, RegNav, ForgotNav} from '../screens';
+import { BackHandler } from 'react-native';
 
 
 const Stack = createStackNavigator();
@@ -10,10 +11,10 @@ export default function LogNav() {
 
   return (
 
-    <Stack.Navigator initialRouteName={routes.LOGIN} //creates navigator for login
+    <Stack.Navigator initialRouteName={routes.LOGIN} screenOptions={BackHandler} //creates navigator for login
     >
       <Stack.Group>
-      <Stack.Screen name={routes.LOGIN} component={Login} options={{headerShown:false}} //login page
+      <Stack.Screen name={routes.LOGIN} component={Login} options={{headerShown:false, gestureEnabled:false}} //login page
       />
       </Stack.Group>
 
@@ -30,7 +31,7 @@ export default function LogNav() {
       
       <Stack.Group>
 
-      <Stack.Screen name={routes.MENU_NAVIGATOR} component={MenuNav} options={{headerShown:false}}  //Home page with the menu navigator
+      <Stack.Screen name={routes.MENU_NAVIGATOR} component={MenuNav} options={{headerShown:false,  gestureEnabled:false}}  //Home page with the menu navigator
       />
       </Stack.Group>
     </Stack.Navigator>
--- src/screens/Home/AccountSetting.js
+++ src/screens/Home/AccountSetting.js
@@ -4,6 +4,7 @@ import color from '../../config/color';
 import routes from '../../config/routes';
 import * as ImagePicker from "expo-image-picker";
 import { Linking } from 'react-native';
+import { Alert } from 'react-native';
 
 //Account setting page where user can change password or change profile pic
 
@@ -13,7 +14,12 @@ function AccountSetting({navigation}) {
     const[image, setImage] = useState(null);
 
     const [perm, setPerm] = useState(null);
+    const [permCam, setCamPerm] = useState(null);
+
+
     const [imgUrl, setImgUrl] = useState(false);
+
+
     
     useEffect(() =>{
             (async()=> {
@@ -34,17 +40,46 @@ function AccountSetting({navigation}) {
             // const {status} = ImagePicker.requestMediaLibraryPermissionsAsync();
 
         }, []);
+
+    useEffect(() =>{
+            (async()=> {
+                if(Platform.OS === "ios"){
+                    const {status} = await ImagePicker.requestCameraPermissionsAsync();
+                    console.log("status:",status);
+                    if(status !== "granted"){
+                        setCamPerm("denied");
+                        alert("Permission not granted")
+                        console.log("perm:",permCam)
+                    }
+                    else{
+                        setCamPerm("granted");
+                        console.log("perm:",permCam)
+                    }
+                }
+            })();
+            // const {status} = ImagePicker.requestMediaLibraryPermissionsAsync();
+
+        }, []);
         
 
     const access = async()=> {useEffect(() =>{
         (async()=> {
-            console.log("wowooowowoow");
             const {status} = await ImagePicker.getMediaLibraryPermissionsAsync();
             setPerm(status);
             console.log("new status:", status);
             console.log("new perm:", perm);
         })();
     })};
+
+    const accessCam = async()=> {useEffect(() =>{
+        (async()=> {
+            const {status} = await ImagePicker.getCameraPermissionsAsync();
+            setCamPerm(status);
+            console.log("new status:", status);
+            console.log("new perm:", permCam);
+        })();
+    })};
+
 
 
     const pickImage = async() =>{
@@ -68,61 +103,98 @@ function AccountSetting({navigation}) {
             access();
             
     }}
+    const camImage = async() =>{
+        
+        
+        if(permCam == "granted"){
+            let picture = await ImagePicker.launchCameraAsync();
+            console.log("result", picture)
+
+
+            if(!picture.canceled){
+                setImgUrl(true)
+                setImage(picture.uri)
+            }
+        }
+        else{
+            Linking.openSettings();
+            access();
+            
+    }}
 
     
 
     return (
 
-        <SafeAreaView style ={styles.back}>
 
-            <View style ={styles.mainBorder}>
+        <SafeAreaView style ={styles.mainBorder}>
+            <View style= {styles.pfp}>
 
                 <View style ={styles.imageBorder}>
-                    <Image  style ={styles.logo}
-                    source={imgUrl ? {uri: image}: require("../../assests/user.png")} //our logo
+                    
+                        <Image  style ={styles.logo}
+                        source={imgUrl ? {uri: image}: require("../../assests/user.png")} //our logo
                 />
                 </View>
-                    <Button 
-                    title="Change Password" //change password button
-                    onPress={()=> navigation.navigate(routes.NEW_PASSWORD)} />
+                <View style= {styles.camera}>
+                    <TouchableOpacity style={styles.click} onPress={camImage}>
+                        <Image style={styles.icon} source={require("../../assests/camera.png")}/>
+                    </TouchableOpacity>
+                </View>
+
+            </View>
+                   
                 <View style ={styles.settingBorder}>
                     
-                
+                 <Button 
+                    title="Change Password" //change password button
+                    onPress={()=> navigation.navigate(routes.NEW_PASSWORD)} />
                 
                 <Button 
                     title = "Change Profile Picture" //change picture button
                     onPress={pickImage}
                 />
+
+                <Button 
+                    title = "Sign Out" //change picture button
+                />
+
+
+
+
                 </View>
                 
-            </View>
         </SafeAreaView>
     );
 }
 const styles = StyleSheet.create({
-    back:{ //background
-        backgroundColor: color.third,
-        flex:1,
-        justifyContent:"center",
-        alignItems:"center",
-    }
-    ,
+
+    
     mainBorder:{ //main border holds everything
         width:"100%",
         height:"100%",
+        flex:1,
         backgroundColor:color.primary,
         alignItems:"center",
         justifyContent:"space-evenly",
     },
+    pfp:{
+        width:"63%",
+        height:"35%",
+        alignItems: "center"
+    },
     imageBorder:{ //border where the profile pic willl be placed
-        width:"53%",
-        height:"30%",
-        borderRadius:25,
-        backgroundColor: color.third,
+        width:"90%",
+        height:"90%",
+        borderRadius:20,
+        backgroundColor: "#ffd",
         justifyContent: 'center',
+        borderWidth:2,
+        borderColor: color.yellow,
         alignItems: 'center',
         paddingTop: "2.5%",
         paddingBottom: "2.5%",
+        flexWrap:"nowrap"
     },
     settingBorder:{ //border for the options to change password or picture
         width: "100%",
@@ -133,9 +205,38 @@ const styles = StyleSheet.create({
     logo:{
         flex:1,
         width:'90%',
-        height: '90%',
-        borderRadius: 25
+        height: '100%',
+        borderWidth:2, 
+        borderColor: color.primary,
+        borderRadius: 18
+    },
+    camera:{
+        width:"13%",
+        height:"13%",
+        backgroundColor:color.yellow,
+        borderRadius: 20,
+        position: "absolute",
+        right: 12,
+        bottom:12,
+        alignItems:"center",
+        justifyContent:"center",
+        flex:1,
+    },
+    icon:{
+        width:"80%",
+        height:"80%",
+        position:'absolute',
+        left: 2,
+        top:2,
+
+    },
+    click:{
+        width:"80%",
+        height:"60%",
+        position:'absolute'
+
     }
+
 
 })
 
