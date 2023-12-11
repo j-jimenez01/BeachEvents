5ff9098324dff44db5444dc95179cf642d97fb0d
DhruvGorasiya <Dhruv.Gorasiya@student.csulb.edu>
2023-12-11 03:53:13 -0800

    locations updated

---
+++ node_modules 3.52.02 AM/react-navigation/index.js
@@ -0,0 +1,1 @@
+throw new Error('The `react-navigation` package is no longer supported. See https://reactnavigation.org/docs/getting-started for up-to-date documentation on how to install and use the latest version of React Navigation.')
